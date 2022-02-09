af9ccfc9ae4720ce4abd02dbf03dc69a8452223b
Brenda-CW <factotum4rays@gmail.com>
2022-02-09 13:01:37 -0700

    JS button function in progress

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
-const btn = document.getElementById("#btn"),
-const firstName = document.getElementById("#firstName"),
-const firstError = document.getElementById("firstNameError")
-const lastName = document.getElementById("lastName"),
-const lastError = document.getElementById("lastNameError"),
-const email = document.getElementById("email"),
-const emailError = document.getElementById("emailError"),
-const password = document.getElementById("password"),
-const passwordError = document.getElementById("passwordError"),
+import"./styles.css";
+
+const btn = document.getElementById("btn");
+const firstName = document.getElementById("first-name");
+const firstError = document.getElementById("first-name-error");
+const lastName = document.getElementById("last-name");
+const lastError = document.getElementById("last-name-error");
+const email = document.getElementById("email");
+const emailError = document.getElementById("email-error");
+const password = document.getElementById("password");
+const passwordError = document.getElementById("password-error");
 
 const submit = () => {
-    console.log(firstName)
-    if (firstName === "") {
-        firstName.innerHTML = <img>"../icon-error.svg"</img>
-        firstError.innerHTML = "<p>First Name cannot be empty.</p>"
+    const first = firstName.value;
+    const last = lastName.value;
+    console.log(first)
+    console.log(last)
+    if (first === "") {
+        firstName.innerHTML = "<img>'./images/icon-error.svg'</img>"
+        firstError.classList.remove(".hide") 
         console.log("First Name Error!")
-    } else if (lastName === "") {
-        lastError.innerHTML = "<p>Last Name cannot be empty.</p>"
+    } else if (last === "") {
+        lastError.classList.remove(".hide") 
         console.log("Last Name Error!")
     }
 }
